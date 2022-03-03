812640ab282201865ea6daf69c15fca0adb3af42
james10911178 <100347704+james10911178@users.noreply.github.com>
2022-03-03 19:17:12 +0800

    Create 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){   
+    int myAge=16;
+
+    int Myweight=100;
+
+    int Myheight=176;
+
+    int Myolderbrotherage=18;
+
+    printf("I am %d years old \n",myAge);
+    
+    printf("My weight is %dkg \n",Myweight);
+    
+    printf("My height is %dcm \n",Myheight);
+    
+    printf("I have older brother,he is %d years old \n",Myolderbrotherage);
+    
+    printf("Me:I have a qustion? \n");
+    
+    printf("Brother:What? \n");
+    
+    printf("Do you speak japanese? \n");
+
+    printf("fuck you nigger. \n");
+    
+    return 0;
+}
