ec16f5ccff176eafb3479b409665eb775a64da35
james10911178 <10911178@student.kjsh.ntpc.edu.tw>
2022-03-17 10:03:59 +0000

    update

---
+++ ex4.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int a, b, product;
+    a=30,b=5;
+    product =a*b;
+
+
+    printf("a*b=%d \n",product);
+
+
+    product =a/b;
+
+
+    printf("a/b=%d \n",product);
+    
+    return 0;
+}
