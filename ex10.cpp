9f2fa78deb1e749745b2db2798b3000195992b15
james10911178 <10911178@student.kjsh.ntpc.edu.tw>
2022-04-07 11:02:48 +0000

    update

---
+++ ex10.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int i, j;
+    for(j=1;j<8;j++){
+        for(i=1;i<=j;i++){
+        printf(" %d",j);
+        //cout << i << "*" << j << "= " << i*j <<" " ;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
