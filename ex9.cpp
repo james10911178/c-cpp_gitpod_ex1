f0737eb5d22ea66faf7d342707404f66836b5d05
james10911178 <10911178@student.kjsh.ntpc.edu.tw>
2022-04-07 09:49:35 +0000

    update

---
+++ ex9.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int i, j;
+    for(i=1;i<10;i++){
+
+        cout << " i=" << i << endl;
+    }
+
+    cout<<"Hello World";
+
+    return 0;
+}
