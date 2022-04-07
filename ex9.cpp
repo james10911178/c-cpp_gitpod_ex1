400ce86e7c45e320bc65d9313daf1c5ecc348b31
james10911178 <10911178@student.kjsh.ntpc.edu.tw>
2022-04-07 10:45:48 +0000

    update

--- ex9.cpp
+++ ex9.cpp
@@ -6,9 +6,11 @@ int main()
 {
     int i, j;
     for(i=1;i<10;i++){
-        for(j=1;j<10;j++)
-
-        cout << i << "*" << j << "=" << i*j <<endl;
+        for(j=1;j<10;j++){
+        printf(" %d*%d = %2d ",i,j,i*j);
+        //cout << i << "*" << j << "= " << i*j <<" " ;
+        }
+        cout << endl;
     }
 
     return 0;
