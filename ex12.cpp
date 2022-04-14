8fbfd2b0b9aacbfd84d2461fc991363489e9205d
james10911178 <10911178@student.kjsh.ntpc.edu.tw>
2022-04-14 09:41:46 +0000

    update

---
+++ ex12.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int i[9] = {1,2,3,5,6,9,8,7,4};
+    int ii;
+
+    for(ii=0;ii < 9;ii++){
+        cout << "i[" << ii << "]=" << i[ii] << endl;
+    }
+
+    cout << "Hello World";
+    return 0;
+}
