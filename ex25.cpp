0c8a9d1374d82190ed14d7db827cfd97890b9315
james10911178 <10911178@student.kjsh.ntpc.edu.tw>
2022-06-16 11:55:06 +0000

    update

--- ex25.cpp
+++ ex25.cpp
@@ -6,22 +6,25 @@ typedef struct Time{
     int min;
 } ttime;
 
-ttime convert(ttime t1){
-    
-    if(t1.hr = 14 && 16){
-        printf("下午茶時間");
-    }else if(t1.min = 20){
-        printf("下午茶時間");
+ttime convert(ttime t){
+    if(t.min > 60){
+        int temp;
+        temp = t.min % 60;
+        t.hr = t.hr + (t.min)/60;
+        t.min = temp;
+    }
+    if(t.hr < 15 && t.min < 20 || t.hr > 15 && t.min > 20){
+        printf("不是下午茶時間");
     }else
-    printf("不是下午茶時間");
+    printf("下午茶時間");
     
-    return t1;  
+    return t;  
 }
 int main() {
     ttime t1, t;
    
-    t1.hr = 2;
-    t1.min = 60;
+    t1.hr = 14
+    t1.min = 20;
     t1 = convert(t1);
     printf(" correct time expression  =  %d hr %d min \n",t1.hr,t1.min);
 }
